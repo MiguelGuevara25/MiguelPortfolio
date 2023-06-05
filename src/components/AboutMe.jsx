c673e04e4765998fc30cdb81dc4051c1df2ddaab
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-06-05 17:39:39 -0500

    DEPLOYMENT :rocket:

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,9 @@ const AboutMe = () => {
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center my-5 text-white flex-col">
-        <p className="text-2xl text-yellow-400 font-bold mt-10">Hello Im</p>
+        <p className="text-2xl text-yellow-400 font-bold mt-10">
+          Hello I&apos;m
+        </p>
         <Logo />
 
         <div className="flex md:gap-0 gap-8 md:flex-row flex-col justify-evenly items-center my-10">
@@ -14,15 +16,16 @@ const AboutMe = () => {
           </div>
 
           <p className="w-11/12 md:w-1/2 border-red-500">
-            Hello! Im Miguel Guevara, a Systems Engineering student and frontend
-            developer. My goal is to become a fullstack developer in the future.
-            I specialize in HTML, CSS, and JavaScript to create appealing and
-            functional interfaces. <br /> <br /> Im always learning and staying
-            updated on the latest web development trends and technologies. Im
-            passionate about user-centered design and strive to ensure usability
-            and accessibility in my projects. Im excited to work in
-            collaborative teams and take on new challenges to grow as a
-            professional in the frontend development field.
+            Hello! I&apos;m Miguel Guevara, a Systems Engineering student and
+            frontend developer. My goal is to become a fullstack developer in
+            the future. I specialize in HTML, CSS, and JavaScript to create
+            appealing and functional interfaces. <br /> <br /> I&apos;m always
+            learning and staying updated on the latest web development trends
+            and technologies. I&apos;m passionate about user-centered design and
+            strive to ensure usability and accessibility in my projects.
+            I&apos;m excited to work in collaborative teams and take on new
+            challenges to grow as a professional in the frontend development
+            field.
           </p>
         </div>
       </div>
