5e98dfa683ffa0711228191ab0d1301684e28234
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-06-09 12:31:56 -0500

    DEPLOYMENT :rocket:

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,23 +2,25 @@ import TypeWriterEffect from "react-typewriter-effect";
 
 const Logo = () => {
   return (
-    <TypeWriterEffect
-      cursorColor="transparent"
-      multiText={[
-        "MIGUEL GUEVARA",
-        "FRONTEND DEVELOPER",
-        "DEVELOPER REACT JR.",
-      ]}
-      multiTextLoop={true}
-      multiTextDelay={1000}
-      typeSpeed={80}
-      textStyle={{
-        textAlign: "center",
-        color: "#fff",
-        fontSize: "4rem",
-        fontWeight: "bold",
-      }}
-    />
+    <div className="h-48 md:h-max">
+      <TypeWriterEffect
+        cursorColor="transparent"
+        multiText={[
+          "MIGUEL GUEVARA",
+          "FRONTEND DEVELOPER",
+          "DEVELOPER REACT JR.",
+        ]}
+        multiTextLoop={true}
+        multiTextDelay={1000}
+        typeSpeed={80}
+        textStyle={{
+          textAlign: "center",
+          color: "#fff",
+          fontSize: "4rem",
+          fontWeight: "bold",
+        }}
+      />
+    </div>
   );
 };
 
